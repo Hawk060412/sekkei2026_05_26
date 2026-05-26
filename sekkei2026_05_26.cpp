0a6831ff4f6493d40e3e15f6c63f65f7cdb24986
4D-21\koyo <a>
2026-05-26 15:05:04 +0900

    構造体を使ってプレイヤーの設定をする

--- sekkei2026_05_26.cpp
+++ sekkei2026_05_26.cpp
@@ -1,6 +1,53 @@
 ﻿#include <iostream>
 
 
-int main() {
+struct PlayerSettings {
+	int HP;
+	int MP;
+	int ATK;
+	int DEF;
+	int SPD;
+};
+
+PlayerSettings hero = {
+	1000,100,100,50,10
+};
+
+
+
+class Player {
+public:
+	std::string Name;
+	PlayerSettings Settings;
+
 	
+
+	Player(const Player& other)
+		:Player(other.Name, other.Settings)
+	{
+
+	}
+
+	Player(std::string name,const PlayerSettings& settings)
+		:Name(name), Settings(settings)
+	{
+	}
+
+
+};
+
+
+void Show(const Player& player) {
+	std::cout << "Name: " << player.Name <<
+		"HP: " << player.Settings.HP <<
+		"MP: " << player.Settings.MP <<
+		"ATK: " << player.Settings.ATK <<
+		"DEF: " << player.Settings.DEF <<
+		"SPD: " << player.Settings.SPD << std::endl;
+}
+
+int main() {
+	Player player("勇者", hero);
+
+	Show(player);
 }
